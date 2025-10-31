f19c2767fec1e368e620d80a598e67b344e821bc
Ephraim <ihenachoephraim3@gmail.com>
2025-10-31 12:43:44 +0100

    comment routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import {clerkMiddleware} from "@clerk/express";
 
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 
 import { ENV } from "./config/env.js";
@@ -20,6 +21,7 @@ app.get("/", (req, res) => res.send("Boomer Backend is running"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 
 
