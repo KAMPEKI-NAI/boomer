d560b66b8317fee940bff4c2aa9cd20d3c900599
Ephraim <ihenachoephraim3@gmail.com>
2025-11-03 15:06:32 +0100

    server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,38 @@
 import express from "express";
 import cors from "cors";
-import {clerkMiddleware} from "@clerk/express";
+import { clerkMiddleware } from "@clerk/express";
 
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import notificationRoutes from "./routes/notification.route.js";
 
-
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
-app.get("/", (req, res) => res.send("Boomer Backend is running"));
+app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
 
-
-  const startServer = async () => {
+const startServer = async () => {
   try {
     await connectDB();
 
@@ -44,4 +48,5 @@ app.use("/api/notifications", notificationRoutes);
 
 startServer();
 
+// export for vercel
 export default app;
