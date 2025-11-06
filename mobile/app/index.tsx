317a4bea74587b71e6fc518c2ef8b6714eb0efe9
Ephraim <ihenachoephraim3@gmail.com>
2025-11-06 17:04:16 +0100

    auth-implemented

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native";
+import {useClerk} from '@clerk/clerk-expo';
+import {View, Text, Button } from 'react-native';
 
-export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
-}
+const HomeScreen = () => {
+    const { signOut, user } = useClerk();
+
+    return (
+        <View>
+            <Text>Home Screen</Text>
+
+            <Button onPress={() => signOut()} title="logout"></Button>
+        </View>
+    );
+};
+
+export default HomeScreen;
