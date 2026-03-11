edfcab48799dfb4667e82cdd8385a8a64ccd3ed8
Ephraim <ihenachoephraim3@gmail.com>
2026-03-11 18:47:51 +0100

    debug

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+// Do not use path.resolve or url here, keep it simple
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+});
