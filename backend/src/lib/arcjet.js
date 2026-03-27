724c9759d398f284ca851aec708716b9d648885f
Ephraim <ihenachoephraim3@gmail.com>
2026-03-27 12:10:33 +0100

    ee

--- backend/src/lib/arcjet.js
+++ backend/src/lib/arcjet.js
@@ -1,6 +1,6 @@
 import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
 
-import { ENV } from "./env.js";
+import { ENV } from "..config/env.js";
 
 const aj = arcjet({
   key: ENV.ARCJET_KEY,
