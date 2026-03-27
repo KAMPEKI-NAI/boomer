75f42ddd354c6f0b06a5bf8611d5acf281231cc2
Ephraim <ihenachoephraim3@gmail.com>
2026-03-27 13:19:08 +0100

    gg

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -1,7 +1,7 @@
 import aj from "../lib/arcjet.js";
 import { isSpoofedBot } from "@arcjet/inspect";
 
-export const arcjetProtection = async (req, res, next) => {
+export const arcjetMiddleware = async (req, res, next) => {
   try {
     const decision = await aj.protect(req);
 
