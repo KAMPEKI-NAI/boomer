1599a79627f8a583fa5049d4754157c339411c37
Ephraim <ihenachoephraim3@gmail.com>
2026-03-27 12:04:48 +0100

    arcjet

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -1,32 +1,33 @@
-import arcjet, { detectBot } from "@arcjet/node";
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  rules: [
-    detectBot({
-      mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE", 
-        "JAVA_OKHTTP"],
-    }),
-  ],
-});
-
-export const arcjetMiddleware = async (req, res, next) => {
+export const arcjetProtection = async (req, res, next) => {
   try {
     const decision = await aj.protect(req);
 
-    console.log("Arcjet decision:", decision.reason);
-
     if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({ message: "Rate limit exceeded. Please try again later." });
+      } else if (decision.reason.isBot()) {
+        return res.status(403).json({ message: "Bot access denied." });
+      } else {
+        return res.status(403).json({
+          message: "Access denied by security policy.",
+        });
+      }
+    }
+
+    // check for spoofed bots
+    if (decision.results.some(isSpoofedBot)) {
       return res.status(403).json({
-        error: "Blocked by Arcjet",
-        reason: decision.reason,
+        error: "Spoofed bot detected",
+        message: "Malicious bot activity detected.",
       });
     }
 
     next();
-  } catch (err) {
-    console.error("Arcjet error:", err);
-    next(); // do not block if Arcjet fails
+  } catch (error) {
+    console.log("Arcjet Protection Error:", error);
+    next();
   }
 };
